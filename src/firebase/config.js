13c14454cedf4f89ff7f6377e31328be7f0db462
lanewssports <jhonatanm255@hotmail.com>
2025-04-02 11:11:43 -0600

    reconfiguracion de las variables de entorno (.env)

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -12,6 +12,7 @@ const firebaseConfig = {
   appId: import.meta.env.VITE_FIREBASE_APP_ID || "mock-app-id",
 };
 
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig)
 const auth = getAuth(app)
